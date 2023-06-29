aaa5b96cb90c3c18951114097d505aac031b40c7
Alex Huaitiao <alexaniasco@outlook.com>
2023-06-29 09:56:19 -0300

    upd2

--- src/ServiciosDos.jsx
+++ src/ServiciosDos.jsx
@@ -3,18 +3,38 @@ import React from 'react'
 export const ServiciosDos = () => {
     return (
         <div className='serviciosDos' id='tres'>
+            <p className='tit_apologia'>Apología del éxito en la  Web</p>
             <div className='serviciosDosHead'>
                 <div className='serviciosDosText'>
-                    <p style={{ fontSize: "52px", textAlign: "start", fontWeight: "700", margin: "0" }}>Apología del éxito en la  Web</p>
-                    <p className='p2' style={{ marginTop: "20px" }}>Somos los únicos desarrolladores comprometidos en hacer que la web sea accesible para todos los usuarios, sin importar su presupuesto.</p>
-                    <button style={{ borderRadius: "2rem", backgroundColor: "#FA72A8", color: "white", marginTop: "70px" }}>Comienza ahora</button>
+                    <div className='tarjeta1'>
+                        <p className='p2'>Somos los únicos desarrolladores comprometidos en hacer que la web sea accesible para todos los usuarios, sin importar su presupuesto.</p>
+                    </div>
                 </div>
                 <div className='servidiosDosImagenCont'>
-
                     <img className='serviciosDosImagen' id='pc' src="/pc.svg" alt="" />
                 </div>
             </div>
-         
+            <div className='serviciosDosHead'>
+                <div className='servidiosDosImagenCont2'>
+                    <img className='serviciosDosImagen2' id='pc' src="/dardo.svg" alt="" />
+                </div>
+                <div className='serviciosDosText'>
+                    <div className='tarjeta1'>
+                        <p className='p2'>Poder vender en la web no es dificil y cada vez hay mas formas de hacerlo, pero aun nada se compara con tener un sitio a tu medida donde esta el 100% de los beneficios.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='serviciosDosHead'>
+                <div className='serviciosDosText'>
+                    <div className='tarjeta1'>
+                        <p className='p2'>Hay que estar claros en el proceso personalizado que se requiere. Cada emprendimiento es un proyecto para  el cual creamos interfaces atractivas e intuitivas con análisis del mercado y testeo previo.</p>
+                    </div>
+                </div>
+                <div className='servidiosDosImagenCont2'>
+                    <img className='serviciosDosImagen3' id='pc' src="/estadisticas.svg" alt="" />
+                </div>
+            </div>
+
         </div>
     )
 }
